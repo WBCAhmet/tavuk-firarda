e6c6b7cfbe0dd53d934d5059d60674cb459c992e
Ahmet Akkuş <ahmedakkus.42@gmail.com>
2024-08-14 21:05:09 +0300

    Coroutine alıştırması ve reset

--- Assets/Scripts/Managers/GameDirector.cs
+++ Assets/Scripts/Managers/GameDirector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,35 @@ public class GameDirector : MonoBehaviour
 
     public MapGenerator mapGenerator;
 
+    public Player player;
+
+    public CameraHolder cameraHolder;
+
     // Start is called before the first frame update
     void Start()
     {
         mapGenerator.StartMapGenerator();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)) {
+            RestartLevel();
+        }
+    }
+
+    private void RestartLevel()
+    {
+        // Player Position ResetLe
+        player.ResetPlayer();
+        // Kameranýn pozisyonunu resetle
+        cameraHolder.ResetCameraHolder();
+        // Haritayý Sil
+        // Haritayý Yeniden Oluþtur
+        // Player Skorunu 0la
+        
+
+
     }
 
     public void UpdatePlayerScore(int playerScore)

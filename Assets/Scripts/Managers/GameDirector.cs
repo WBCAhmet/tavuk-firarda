70a82e615071b04c77c08777c6fdb1ca643e6ebc
Ahmet Akkuş <ahmedakkus.42@gmail.com>
2024-08-28 21:04:39 +0300

    coin sistemi eklendi. satır silinmesi eklendi

--- Assets/Scripts/Managers/GameDirector.cs
+++ Assets/Scripts/Managers/GameDirector.cs
@@ -7,6 +7,7 @@ public class GameDirector : MonoBehaviour
 {
     [Header("Managers")]
     public MapGenerator mapGenerator;
+    public CoinManager coinManager;
 
     [Header("UI")]
     public PlayerScoreUI playerScoreUI;
@@ -32,6 +33,30 @@ public class GameDirector : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             RestartLevel();
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            coinManager.EarnCoins(1);
+            print(coinManager.GetCoinCount());
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            coinManager.ResetCoinCount();
+            print(coinManager.GetCoinCount());
+        }
+    }
+
+    public void DeleteRow(int rowCount)
+    {
+        StartCoroutine(DeleteRowCoroutine(rowCount));
+    }
+
+    IEnumerator DeleteRowCoroutine(int rowCount)
+    {
+        for (int i = 0; i < rowCount; i++)
+        {
+            mapGenerator.DeleteRow();
+            yield return new WaitForSeconds(.1f);
         }
     }
 
